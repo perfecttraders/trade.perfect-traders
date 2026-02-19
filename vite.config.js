4c64e9b3faed153655c4b38ad33bf2f3b6f70438
perfecttraders <chandwanirohit94@gmail.com>
2026-02-19 21:08:09 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,4 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
-  base: '/trade.perfect-traders/', // must match repo name
+  base: '/trade.perfect-traders/',
 })
