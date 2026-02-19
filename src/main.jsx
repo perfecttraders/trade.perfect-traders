d5375761d572b10c0aa0f18b38eca26fdd28e546
perfecttraders <chandwanirohit94@gmail.com>
2026-02-19 20:50:08 +0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+<HashRouter>
+  <App />
+</HashRouter>
 import App from './App'
 import './styles.css'
 
